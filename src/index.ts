682b2b8992e408604cab7a783d057bdf4d76ef73
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-02-21 18:26:18 -0300

    fix: minor seo improvements

--- src/index.ts
+++ src/index.ts
@@ -32,11 +32,13 @@ const server = http.createServer(async (req, res) => {
   const content =
     `
   <!DOCTYPE html>
-  <html lang="en">
+  <html lang="es">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index,nofollow,noarchive" />
+    <meta name="description" content="A web developer website">
     <title>Gabiland Republic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
