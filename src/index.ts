c85a36a5ca303368125d7ccd1f2e4fb1fc46a1fa
Gabriel De Los Rios <gab.delosrios@gmail.com>
2024-05-02 22:51:22 -0300

    fix: avoids main content leak into static files serving

--- src/index.ts
+++ src/index.ts
@@ -68,84 +68,112 @@ const server = http.createServer(async (req, res) => {
         logError(error);
       }
     }
-  }
-  const chatComponent = renderChatComponent();
-  const messagePanelComponent = await renderMessagePanelComponent();
-  const spotifyComponent = renderSpotifyComponent();
-  // prettier-ignore
-  const content =
-    `
-  <!DOCTYPE html>
-  <html lang="es">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="index,nofollow,noarchive" />
-    <meta name="description" content="A web developer website">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://*; child-src 'none'; script-src 'unsafe-inline' https://cdn.scaledrone.com/scaledrone.min.js; style-src-elem https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css 'unsafe-inline'; worker-src 'self'; connect-src 'self' wss://api.scaledrone.com/v3/websocket;" />
-    <title>Gabiland Republic</title>
-    <link rel="preconnect" href="wss://api.scaledrone.com/v3/websocket">
-    <link rel="preconnect" href="https://res.cloudinary.com">
-    <link rel="preconnect" href="https://cdn.jsdelivr.net">
-    <link rel="icon" type="ico" href="https://res.cloudinary.com/programming-web-venture/image/upload/v1677117169/favicon_ioevdp.ico"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
-  <style>
-   ::-webkit-scrollbar {
-      width: 20px;
-    }
-    ::-webkit-scrollbar-track {
-      background-color: transparent;
-    }
-    ::-webkit-scrollbar-thumb {
-      background-color: #d6dee1;
-    }
-    ::-webkit-scrollbar-thumb {
-      background-color: #d6dee1;
-      border-radius: 20px;
-    }
-    ::-webkit-scrollbar-thumb {
-      background-color: #d6dee1;
-      border-radius: 20px;
-      border: 6px solid transparent;
-      background-clip: content-box;
-    }
-    .alert-success {
-      --bs-alert-color: #e71ad2;
-      --bs-alert-bg: rgba(54, 41, 54, 0.9);
-      --bs-alert-border-color: var(--bs-success-border-subtle);
-      --bs-alert-link-color: var(--bs-success-text);
-    }
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      min-height: 100vh;
-      background: radial-gradient(ellipse at bottom, #0d1d31 0%, #0c0d13 100%);
-      overflow: hidden;
-    }
-  </style>
-  <body>
+  } else {
+    const chatComponent = renderChatComponent();
+    const messagePanelComponent = await renderMessagePanelComponent();
+    const spotifyComponent = renderSpotifyComponent();
+    // prettier-ignore
+    const content =
+      `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="robots" content="index,nofollow,noarchive" />
+      <meta name="description" content="A web developer website">
+      <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://*; child-src 'none'; script-src 'unsafe-inline' https://cdn.scaledrone.com/scaledrone.min.js; style-src-elem https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css 'unsafe-inline'; worker-src 'self'; connect-src 'self' wss://api.scaledrone.com/v3/websocket;" />
+      <title>Gabiland Republic</title>
+      <link rel="preconnect" href="wss://api.scaledrone.com/v3/websocket">
+      <link rel="preconnect" href="https://res.cloudinary.com">
+      <link rel="preconnect" href="https://cdnjs.cloudflare.com">
+      <link rel="icon" type="ico" href="https://res.cloudinary.com/programming-web-venture/image/upload/v1677117169/favicon_ioevdp.ico"/>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <style>
+     ::-webkit-scrollbar {
+        width: 20px;
+      }
+      ::-webkit-scrollbar-track {
+        background-color: transparent;
+      }
+      ::-webkit-scrollbar-thumb {
+        background-color: #d6dee1;
+      }
+      ::-webkit-scrollbar-thumb {
+        background-color: #d6dee1;
+        border-radius: 20px;
+      }
+      ::-webkit-scrollbar-thumb {
+        background-color: #d6dee1;
+        border-radius: 20px;
+        border: 6px solid transparent;
+        background-clip: content-box;
+      }
+      .alert-success {
+        --bs-alert-color: #e71ad2;
+        --bs-alert-bg: rgba(54, 41, 54, 0.9);
+        --bs-alert-border-color: var(--bs-success-border-subtle);
+        --bs-alert-link-color: var(--bs-success-text);
+      }
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background: radial-gradient(ellipse at bottom, #0d1d31 0%, #0c0d13 100%);
+        overflow: hidden;
+      }
+      .notification-box {
+        position: fixed; 
+        bottom: 0;
+        width: 100%;
+        padding: 5px;
+        background: #9b8861;;
+        color: white;
+        font-size: 1.2rem;
+        overflow: hidden;
+        max-height: 35px;
+        .notification-text {
+          line-height: 25px;
+          white-space: nowrap;
+          animation: slideText 10s linear infinite;
+          @media(min-width: 768px){
+            animation: none;
+          }
+        }
+      }
+      @keyframes slideText {
+        0% {
+          transform: translateX(100%);
+        }
+        100% {
+          transform: translateX(-100%);
+        }
+      }
+    </style>
+    <body>
+    <div class="notification-box"><div class="notification-text"><b>Gabilandia stands with Palestine 🇵🇸</b> - <em>STOP GENOCIDE</em> - <em>CEASEFIRE NOW!</em></div></div>
   
-
-    ` + chatComponent + `
-
-    <div class="container vh-100">
-        ` + spotifyComponent + `
-      <main class="d-flex align-items-center h-100 p-4">
-        ` + messagePanelComponent + `
-      </main>
-    </div>
-  </body>
-  <script>
-    navigator.serviceWorker.register('sw.js');
-    history.pushState({prev:window.location.pathname},'','/');
-    ${webcomponents}
-  </script>
-  </html>
-  `;
-  res.end(content);
+      ` + chatComponent + `
+  
+      <div class="container vh-100">
+          ` + spotifyComponent + `
+        <main class="d-flex align-items-center h-100 p-4">
+          ` + messagePanelComponent + `
+        </main>
+      </div>
+    </body>
+    <script>
+      navigator.serviceWorker.register('sw.js');
+      history.pushState({prev:window.location.pathname},'','/');
+      ${webcomponents}
+    </script>
+    </html>
+    `;
+    res.end(content);
+  }
 });
 
 server.listen(8888, hostname, () => {});
